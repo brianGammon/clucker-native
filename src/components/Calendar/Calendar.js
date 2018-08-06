31bb9300e6a05f4fb64c5df7b3db04d3d954c4c5
Brian Gammon <bgammon@gmail.com>
2018-08-06 15:36:51 -0400

    basic month date switcher controls

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import CalendarRenderer from './CalendarRenderer';
 import eggsByRangeSelector from '../../selectors/eggsByRangeSelector';
 import flockStatsSelector from '../../selectors/flockStatsSelector';
 import { type Egg } from '../../types';
-import { nowAsMoment } from '../../utils/dateHelper';
+import { nowAsMoment, dateStringAsMoment } from '../../utils/dateHelper';
 
 type Props = {
   navigation: any,
@@ -12,22 +13,51 @@ type Props = {
     [eggId: string]: Egg,
   },
   stats: any,
+  dates: {
+    date: string,
+    previousDate: string,
+    nextDate: string,
+  },
 };
 
 class Calendar extends React.Component<Props> {
   render() {
-    const { navigation, eggs, stats } = this.props;
+    const {
+      navigation, eggs, stats, dates,
+    } = this.props;
     return (
-      <CalendarRenderer navigation={navigation} eggs={eggs} stats={stats} />
+      <CalendarRenderer
+        navigation={navigation}
+        eggs={eggs}
+        stats={stats}
+        dates={dates}
+      />
     );
   }
 }
 
 const mapStateToProps = ({ eggs }, { navigation }) => {
   const date = navigation.getParam('date', nowAsMoment().format('YYYY-MM'));
+  const currDate = moment.utc(date);
+  const previousDate = currDate
+    .subtract(1, 'months')
+    .format('YYYY-MM')
+    .toString();
+  let nextDate = currDate
+    .add(2, 'months')
+    .format('YYYY-MM')
+    .toString();
+  if (dateStringAsMoment(nextDate).isAfter(nowAsMoment())) {
+    nextDate = null;
+  }
   return {
     eggs: eggsByRangeSelector(eggs.data, date),
     stats: flockStatsSelector(eggs.data, date),
+    dates: {
+      date,
+      previousDate,
+      nextDate,
+    },
   };
 };
 
--- src/components/Calendar/CalendarRenderer.js
+++ src/components/Calendar/CalendarRenderer.js
@@ -1,6 +1,10 @@
+/* @flow */
 import * as React from 'react';
-import { Text, Button, ScrollView } from 'react-native';
+import {
+  Text, Button, ScrollView, View,
+} from 'react-native';
 import { type Egg } from '../../types';
+import styles from './styles';
 
 type Props = {
   navigation: any,
@@ -8,10 +12,30 @@ type Props = {
     [eggId: string]: Egg,
   },
   stats: any,
+  dates: {
+    date: string,
+    previousDate: string,
+    nextDate: string,
+  },
 };
 
-const CalendarRenderer = ({ navigation, eggs, stats }: Props) => (
+const CalendarRenderer = ({
+  navigation, eggs, stats, dates,
+}: Props) => (
   <ScrollView>
+    <View style={styles.dateSwitcher}>
+      <Button
+        onPress={() => navigation.replace('Month', { date: dates.previousDate })
+        }
+        title="Previous"
+      />
+      <Text>{dates.date}</Text>
+      <Button
+        disabled={dates.nextDate === null}
+        onPress={() => navigation.replace('Month', { date: dates.nextDate })}
+        title="Next"
+      />
+    </View>
     <Text style={{ fontSize: 18 }}>Month Calendar</Text>
     <Text style={{ fontSize: 14 }}>Stats</Text>
     <Text style={{ fontSize: 14 }}>{JSON.stringify(stats, null, 2)}</Text>
