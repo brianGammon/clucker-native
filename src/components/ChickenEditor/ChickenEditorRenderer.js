07b9189d06423fe63a588d2164b4d8c3012f0a53
Brian Gammon <bgammon@gmail.com>
2018-09-20 14:27:31 -0400

    allow hatch date to be up to 20 years back (for sophie)

--- src/components/ChickenEditor/ChickenEditorRenderer.js
+++ src/components/ChickenEditor/ChickenEditorRenderer.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {
   Container, Content, View, Text, Form,
 } from 'native-base';
-import moment from 'moment';
 import { Field, FieldGroup, FieldControl } from 'react-reactive-form';
 import FormInput from '../FormInput';
 import ChickenPhotoPicker from '../ChickenPhotoPicker';
 import DatePicker from '../DatePicker';
 import Header from '../Header';
 import styles from './styles';
+import { nowAsMoment } from '../../utils/dateHelper';
 
 type Props = {
   mode: 'Edit' | 'Add',
@@ -44,80 +44,85 @@ const ChickenEditorRenderer = ({
   <FieldGroup
     control={form}
     strict={false}
-    render={({ invalid }) => (
-      <Container>
-        <Header
-          title={`${mode} Chicken`}
-          cancelButton
-          handleSave={handleSubmit}
-          saveDisabled={invalid || inProgress}
-        />
-        <Content>
-          <View padder>
-            {error && <Text style={styles.error}>{error}</Text>}
+    render={({ invalid }) => {
+      const now = nowAsMoment();
+      const minDate = now.clone().subtract(20, 'years');
+      return (
+        <Container>
+          <Header
+            title={`${mode} Chicken`}
+            cancelButton
+            handleSave={handleSubmit}
+            saveDisabled={invalid || inProgress}
+          />
+          <Content>
+            <View padder>
+              {error && <Text style={styles.error}>{error}</Text>}
 
-            <Form>
-              <FieldControl
-                name="name"
-                render={formProps => <FormInput {...formProps} />}
-                meta={{
-                  label: 'Name',
-                  maxLength: 25,
-                  inputRefs,
-                  onSubmitEditing: () => focusNext('Breed'),
-                  returnKeyType: 'next',
-                  blurOnSubmit: false,
-                }}
-              />
+              <Form>
+                <FieldControl
+                  name="name"
+                  render={formProps => <FormInput {...formProps} />}
+                  meta={{
+                    label: 'Name',
+                    maxLength: 25,
+                    inputRefs,
+                    onSubmitEditing: () => focusNext('Breed'),
+                    returnKeyType: 'next',
+                    blurOnSubmit: false,
+                  }}
+                />
 
-              <FieldControl
-                name="breed"
-                render={formProps => <FormInput {...formProps} />}
-                meta={{
-                  label: 'Breed',
-                  maxLength: 25,
-                  inputRefs,
-                  returnKeyType: 'done',
-                  blurOnSubmit: true,
-                }}
-              />
+                <FieldControl
+                  name="breed"
+                  render={formProps => <FormInput {...formProps} />}
+                  meta={{
+                    label: 'Breed',
+                    maxLength: 25,
+                    inputRefs,
+                    returnKeyType: 'done',
+                    blurOnSubmit: true,
+                  }}
+                />
 
-              <Field
-                control={form.get('hatched')}
-                strict={false}
-                render={({ value }) => (
-                  <DatePicker
-                    label="Hatched On"
-                    value={value}
-                    onDateChange={onDateChange}
-                    clearable
-                    maximumDate={new Date(moment().valueOf())}
-                  />
-                )}
-              />
-
-              <Field
-                control={form.get('photoUrl')}
-                strict={false}
-                render={({ value: photoUrl }) => {
-                  const { newImage } = form.value;
-                  return (
-                    <ChickenPhotoPicker
-                      photoUrl={photoUrl}
-                      newImage={newImage}
-                      originalPhotoUrl={originalPhotoUrl}
-                      onRemoveProfilePhoto={onRemoveProfilePhoto}
-                      onResetProfilePhoto={onResetProfilePhoto}
-                      onSelectPhoto={onSelectPhoto}
+                <Field
+                  control={form.get('hatched')}
+                  strict={false}
+                  render={({ value }) => (
+                    <DatePicker
+                      label="Hatched On"
+                      value={value}
+                      onDateChange={onDateChange}
+                      clearable
+                      minimumDate={new Date(minDate.valueOf())}
+                      maximumDate={new Date(now.valueOf())}
                     />
-                  );
-                }}
-              />
-            </Form>
-          </View>
-        </Content>
-      </Container>
-    )}
+                  )}
+                />
+
+                <Field
+                  control={form.get('photoUrl')}
+                  strict={false}
+                  render={({ value: photoUrl }) => {
+                    const { newImage } = form.value;
+                    return (
+                      <ChickenPhotoPicker
+                        photoUrl={photoUrl}
+                        newImage={newImage}
+                        originalPhotoUrl={originalPhotoUrl}
+                        onRemoveProfilePhoto={onRemoveProfilePhoto}
+                        onResetProfilePhoto={onResetProfilePhoto}
+                        onSelectPhoto={onSelectPhoto}
+                      />
+                    );
+                  }}
+                />
+              </Form>
+            </View>
+          </Content>
+        </Container>
+      );
+    }}
   />
 );
 
